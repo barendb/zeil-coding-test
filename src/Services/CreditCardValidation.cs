451563f5e13a461fcfaac248608c082c08310fae
Barend Bootha <mail@barendb.com>
2025-02-28 15:24:32 +1300

    add creditcardvalidation

---
+++ src/Services/CreditCardValidation.cs
@@ -0,0 +1,36 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Services;
+
+public static class CreditCardValidation
+{
+    // Convert to int.
+    private static readonly Func<char, int> CharToInt = c => c - '0';
+
+    private static readonly Func<int, bool> IsEven = i => i % 2 == 0;
+    
+    private static readonly Func<int, int> DoubleDigit = i => (i * 2).ToString().ToCharArray().Select(CharToInt).Sum();
+    
+    /// <summary>
+    /// Checks that the card number passed in, passes luhn validation
+    /// </summary>
+    /// <param name="creditCardNumber">string</param>
+    /// <returns>bool</returns>
+    /// <exception cref="ArgumentException">expect creditCardNumber to only contain digits</exception>
+    public static bool IsValidLuhn(string creditCardNumber)
+    {
+        if (!Regex.IsMatch(creditCardNumber, @"^\d+$"))
+        {
+            throw new ArgumentException("creditCardNumber should only contain digits: ^\\d+$");
+        }
+        
+        var checkSum = creditCardNumber
+            .ToCharArray()
+            .Select(CharToInt)
+            .Reverse()
+            .Select((digit, index) => IsEven(index + 1) ? DoubleDigit(digit) : digit)
+            .Sum();
+
+        return checkSum % 10 == 0;
+    }
+}
