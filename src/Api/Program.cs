b3e56abee559662d96a2de661d79449bb6556a35
Barend Bootha <mail@barendb.com>
2025-03-03 09:04:25 +1300

    add serilog

--- src/Api/Program.cs
+++ src/Api/Program.cs
@@ -1,28 +1,44 @@
-using System.Text.Json;
 using Api.Middleware;
+using Serilog;
 
-var builder = WebApplication.CreateBuilder(args);
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateLogger();
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddControllers();
-builder.Services.AddOpenApi();
-
-var app = builder.Build();
-
-app.UseErrorHandling();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+try
 {
-    app.MapOpenApi();
-    app.UseSwaggerUI(options =>
+    Log.Information("Starting web application");
+    
+    var builder = WebApplication.CreateBuilder(args);
+
+    builder.Host.UseSerilog();
+    builder.Services.AddControllers();
+    builder.Services.AddOpenApi();
+
+    var app = builder.Build();
+
+    app.UseSerilogRequestLogging();
+    app.UseErrorHandling();
+
+    if (app.Environment.IsDevelopment())
     {
-        options.SwaggerEndpoint("/openapi/v1.json", "v1");
-    });
+        app.MapOpenApi();
+        app.UseSwaggerUI(options => { options.SwaggerEndpoint("/openapi/v1.json", "v1"); });
+    }
+
+    // disabled not using any auth
+    // app.UseAuthorization();
+    app.MapControllers();
+    app.UseHttpsRedirection();
+
+    app.Run();
+
 }
-
-app.MapControllers();
-app.UseHttpsRedirection();
-
-app.Run();
+catch (Exception e)
+{
+    Log.Fatal(e, "Unhandled exception");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
